325ce46f3558355d8ac7ee083ac55f0b59bd455f
Cassius W <189533115+CattiusW@users.noreply.github.com>
2026-03-18 15:42:24 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.CONFIG = {
   macros: [
-    { category: 'Entertainment', name: 'YouTube', triggers: ['y', 'yt', 'youtube'], key: 'KeyY', icon: 'youtube', url: 'https://youtube.com', normalisedURL: 'youtube.com', commands: { go: { template: 'https://youtu.be/{$}', description: 'gotovideo' }, search: { template: '{@}/results?search_query={$}' } }, bgColor: { type: 'solid', color: '#f30002' }, textColor: '#212121', pinned: true },
+    { category: 'Entertainment', name: 'Youtube', triggers: ['y', 'yt', 'youtube'], key: 'KeyY', icon: 'youtube', url: 'https://youtube.com', normalisedURL: 'youtube.com', commands: { go: { template: 'https://youtu.be/{$}', description: 'gotovideo' }, search: { template: '{@}/results?search_query={$}' } }, bgColor: { type: 'solid', color: '#f30002' }, textColor: '#212121', pinned: true },
     { category: 'Programming', name: 'GitHub', icon: 'github', url: 'https://github.com', normalisedURL: 'github.com', triggers: ['g', 'git', 'github'], key: 'KeyG', commands: { go: { template: '{@}/{$}', description: 'gotouser' }, search: { template: '{@}/search?q={$}' } }, bgColor: { type: 'solid', color: '#171515' }, textColor: '#e8e8e8', pinned: true },
     { category: 'Programming', name: 'StackOverflow', icon: 'stackoverflow', url: 'https://stackoverflow.com', normalisedURL: 'stackoverflow.com', triggers: ['s', 'st', 'so', 'stack', 'stackoverflow'], key: 'KeyS', commands: { search: { template: '{@}/search?q={$}' }, go: { template: '{@}/questions/{$}', description: 'gotoquestion' } }, bgColor: { type: 'gradient', gradientType: 'linear', colors: ['#e87922', '#ffbe25'], stops: [0, 100] }, textColor: '#212121', pinned: true },
     { category: 'Other', name: 'Translate', icon: 'translate', url: 'https://translate.google.com', normalisedURL: 'translate.google.com', triggers: ['t', 'translate'], key: 'KeyT', commands: { search: { template: '{@}/?text={$}', description: 'translatetext' } }, bgColor: { type: 'gradient', gradientType: 'linear', angle: 45, colors: ['#508bed', '#4654b4'] }, textColor: '#fff', pinned: true },
@@ -11,8 +11,7 @@ window.CONFIG = {
     { category: 'Education', name: 'Wikipedia', icon: 'wikipedia', triggers: ['w', 'wiki', 'wikipedia'], url: 'https://www.wikipedia.org', normalisedURL: 'wikipedia.org', commands: { search: { template: '{@}/?search={$}' }, go: { template: '{@}/wiki/{$}' } }, bgColor: { type: 'gradient', gradientType: 'linear', colors: ['#e1e1e3', '#bbb'], stops: [60, 100] }, textColor: '#000', pinned: true },
     { category: 'Calendar', name: 'SOCS', triggers: ['socs', 'c', 'calendar', 'cms', 'sock'], url: 'https://www.socscms.com/login/161?', normalisedURL: 'www.socscms.com/login/161?', bgColor: { type: 'solid', color: '#e37151' }, textColor: '#e8e8e8' },
     { category: 'Search', name: 'Bing', triggers: ['b', 'bing'], url: 'https://www.bing.com', normalisedURL: 'bing.com', commands: { search: { template: '{@}/search?q={$}' } }, bgColor: { type: 'gradient', gradientType: 'linear', colors: ['#3ab0f1', '#2351dd', '#40d1da'] }, textColor: '#e8e8e8' },
-    { category: 'Search', name: 'Yandex', triggers: ['ya', 'yandex'], url: 'https://ya.ru', normalisedURL: 'ya.ru', commands: { search: { template: '{@}/search/?text={$}' } }, bgColor: { type: 'solid', color: '#fc3f1d' }, textColor: '#000' }
-  ],
+,
   commands: [
     { type: 'search', trigger: '?' },
     { type: 'go', trigger: '/' }
